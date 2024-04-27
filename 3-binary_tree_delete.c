24bbce785b5c680e3a34fb14a5e0a950cdc304c7
uwen_godwin <uwengodwin001@gmail.com>
2024-04-27 08:56:53 +0100

    binary trees

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,12 +1,9 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_delete - deletes an entire binary tree
- *@tree: a pointer to the root node of the tree to delete
- *
- *Return: void
+ * binary_tree_delete - Deletes a binary tree.
+ * @tree: A pointer to the root node of the tree to delete.
  */
-
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree != NULL)
